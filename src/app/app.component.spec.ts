3c87ecde046d4457cecbe75a9371372df0fb973b
Francesco Lombardo <fra.lombardo92@gmail.com>
2020-01-19 18:32:45 +0100

    docker-compose karma service for ci

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -32,12 +32,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ui app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ui app is NOT running!');
   });
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ui app is running!');
-  });
+
 });
